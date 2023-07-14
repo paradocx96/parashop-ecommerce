e4d97f3c8904a412474064c2956c5ecdb07d523b
Navinda Chandrasiri <adilanga@gmail.com>
2023-07-15 00:08:53 +0530

    App Routes Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import NavigationBar from "./components/header/NavigationBar";
 
 export default function App() {
     return (
-        <div className="App">
-            <h1>React App</h1>
-        </div>
+        <>
+            <NavigationBar/>
+            <Routes>
+                <Route exact path="/" element={<Home/>}/>
+                <Route exact path="/cart" element={<Cart/>}/>
+            </Routes>
+        </>
     );
 }
