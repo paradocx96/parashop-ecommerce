42ea81b8ced33e65ccc9ed2c39c4e5b0d6d4eea3
Navinda Chandrasiri <adilanga@gmail.com>
2023-07-15 00:09:11 +0530

    Index Config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import {createRoot} from "react-dom/client";
+import {Provider} from "react-redux";
+import store from "./state/Store";
+import './index.css';
+import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-    <App/>
+    <BrowserRouter>
+        <React.StrictMode>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </React.StrictMode>
+    </BrowserRouter>
 );
