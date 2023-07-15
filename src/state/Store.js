e13cddcd795b3a858fb9bb5c717a345d7bf14776
Navinda Chandrasiri <adilanga@gmail.com>
2023-07-15 17:07:37 +0530

    Store persist config

--- src/state/Store.js
+++ src/state/Store.js
@@ -3,12 +3,24 @@ import mainReducer from "./reducers/mainReducer";
 import thunk from "redux-thunk";
 import {getAllProducts} from "./actions/productActions";
 import {composeWithDevTools} from "redux-devtools-extension";
+import storage from 'redux-persist/lib/storage'
+import {persistReducer, persistStore} from "redux-persist";
+
+
+const persistConfiguration = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfiguration, mainReducer)
 
 const store = createStore(
-    mainReducer,
+    persistedReducer,
     composeWithDevTools(applyMiddleware(thunk))
 )
 
+const persistedStore = persistStore(store);
+
 store.dispatch(getAllProducts());
 
-export default store;
+export {store, persistedStore};
