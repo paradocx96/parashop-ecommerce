2265098b35075a5554633f0906d071feb41a71bc
Navinda Chandrasiri <adilanga@gmail.com>
2023-07-16 14:54:35 +0530

    Cart View Style Updated

--- src/components/cart/CartItems.jsx
+++ src/components/cart/CartItems.jsx
@@ -1,38 +1,43 @@
 import React from 'react';
-import Typography from "@mui/material/Typography";
 import {Link} from "react-router-dom";
-import {clearCart, decrementItemQuantity, incrementItemQuantity, removeItem} from "../../state/actions/cartActions";
+import Typography from "@mui/material/Typography";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import {useDispatch, useSelector} from "react-redux";
-import {cartSelector, cartTotalSelector} from "../../state/selectors/cartSelector";
-import './cart.css';
+import {clearCart, decrementItemQuantity, incrementItemQuantity, removeItem} from "../../state/actions/cartActions";
+import {cartItemsCountSelector, cartSelector, cartTotalSelector} from "../../state/selectors/cartSelector";
+
 
 function CartItems() {
-    const cartItems = useSelector(cartSelector);
-    const cartTotal = useSelector(cartTotalSelector);
     const dispatch = useDispatch();
+    const cartItems = useSelector(cartSelector);
+    const cartItemsCount = useSelector(cartItemsCountSelector);
+    const cartTotal = useSelector(cartTotalSelector);
 
     return (
         <div className="cart-container">
             {/* Cart Header */}
             <div className="cart-header">
-                <Typography variant="h2" noWrap>Shopping Cart</Typography>
+                <Typography variant="h4" noWrap>
+                    <p>Shopping Cart</p>
+                </Typography>
             </div>
 
             {/* Cart Items */}
-            <div>
+            <div className="cart-item-list">
                 {cartItems.items.length === 0 ? (
-                    <div className="empty-cart">
-                        <h3>Cart is empty</h3>
-                        <Link to={'/'}>Start Shopping</Link>
+                    <div className="cart-empty">
+                        <p>Your cart is currently empty</p>
+                        <div className="start-shopping">
+                            <Link to="/">
+                                <ArrowBackIcon/>
+                            </Link>
+                            <Link to="/" style={{textDecoration:"none"}}>
+                                <span>Start Shopping</span>
+                            </Link>
+                        </div>
                     </div>
                 ) : (
                     <div>
-                        {/*<div className="titles">*/}
-                        {/*    <h3>Product</h3>*/}
-                        {/*    <h3>Price</h3>*/}
-                        {/*    <h3>Quantity</h3>*/}
-                        {/*    <h3>Total</h3>*/}
-                        {/*</div>*/}
                         <div className="cart-items">
                             {cartItems.items.map((item) => (
                                 <div className="cart-item" key={item.id}>
@@ -40,13 +45,11 @@ function CartItems() {
                                     <div className="cart-product">
                                         <img src={item.image} alt={item.title}/>
                                         <div>
-                                            <h4>{item.title}</h4>
+                                            <Typography variant="h6">{item.title}</Typography>
+                                            <div className="cart-item-price">${item.price}</div>
                                             <button onClick={() => dispatch(removeItem(item.id))}>Remove</button>
                                         </div>
                                     </div>
-
-                                    {/* Price */}
-                                    <div className="cart-product-price">{item.price}</div>
 
                                     {/* Quantity */}
                                     <div className="cart-product-quantity">
@@ -76,13 +79,15 @@ function CartItems() {
                         <span>Sub Total</span>
                         <span className="amount">${cartTotal}</span>
                     </div>
-                    <p>Tax & Shipping Calculated are Free!</p>
+                    <p>{cartItemsCount} Items</p>
 
                     {/* Checkout */}
                     <button>Checkout</button>
 
                     <div className="continue-shopping">
-                        <Link to={'/'}>Continue Shopping</Link>
+                        <Link to={'/'} style={{textDecoration:"none"}}>
+                            <div>Continue Shopping</div>
+                        </Link>
                     </div>
                 </div>
 
